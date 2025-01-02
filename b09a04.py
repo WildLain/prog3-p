b5b852e517ac98ab59509a54beff913ec70daec8
WildLain <58662815+WildLain@users.noreply.github.com>
2025-01-02 14:13:49 +0000

    Pronomen gefixt

--- b09a04.py
+++ b09a04.py
@@ -19,4 +19,13 @@ print(johnort)
 # (Beispiel: “Herr Meier bekommt mehr, als er verdient!” oder “Frau Biffel bekommt mehr, als sie
 # verdient!”)
 
-schmaeh = [print(line.split(';')[0], line.split(';')[1], "bekommt mehr, als er/sie verdient!") for line in open(filename) if int(line.split(';')[3]) > 100000]
+# schmaeh = [print(line.split(';')[0], line.split(';')[1], "bekommt mehr, als er/sie verdient!") for line in open(filename) if int(line.split(';')[3]) > 100000]
+
+sch = [f"{line[0]} {line[1]} bekommt mehr, als {'er' if line[0].startswith('Herr') else 'sie'} verdient!" for line in [line.strip().split(';') for line in open(filename)] if int(line[3]) >= 100000]
+[print(line) for line in sch]
+
+#Max
+# [f"{x[0]} {x[1]} bekommt mehr, als {"er" if x[0] == "Herr" else "sie"} verdient!" for x in [x[:-1].split(";") for x in list(open("bonz.txt"))] if int(x[3]) >= 100000]
+
+#Larissa
+# print([ f"{part[0]} {part[1]} bekommt mehr, als {'er' if (part[0].startswith('Herr')) else 'sie'} verdient!" for part in [(word[0],word[1],int(word[3])) for word in [word.split(";") for word in [line.strip() for line in open("bonz.txt",'r').readlines()]] if int(word[3]) >= 100000]])
