01ab3fff159d03b2e4f55942ae0abb2092403580
Nha-Dan <trannhadan.tran@student.hs-rm.de>
2025-01-16 14:35:05 +0100

    Blatt 12 Aufgabe 1 Final

--- b12a01.py
+++ b12a01.py
@@ -7,9 +7,10 @@
 # eine Vorkommastelle und optionale, durch Komma getrennte Nachkommastellen (genau zwei). Der
 # Punkt ist als Tausender-Separator erlaubt, also “17.342.915”, “23,56 EUR”, “1425,40 EUR”
 # oder “1.234,56”, nicht aber “1.2.3,56” oder “123.45,97765”.
-# ([1-9]{1,3}.?)(,[\d]{2})?(\sEUR)?
+# ^([1-9]{1,3}\.?)([1-9]{2,3}\.?)*(,\d{2})(\sEUR)?$
 
 # • Telefonnummern: Optional mit Länderkennung (z.B. +49) durch Leerzeichen abgesetzt und ohne 0 bei
 # der Vorwahl. Als Trennzeichen sind Leerezeichen, / und - erlaubt. Zwischen zwei Trennzeichen sind
 # mindestens 2 Ziffern notwendig.
 
+# ^(\+\d{2}[\s/-])[^0](\d{2,}([\s/-]?))*$
