ed1c28b9e1f25140feb2270f82a220d22b93b05e
WildLain <58662815+WildLain@users.noreply.github.com>
2025-01-19 13:00:53 +0000

    a3 Online-Test

--- Online-Test/a3.py
+++ Online-Test/a3.py
@@ -1,19 +1,36 @@
 import re
+
 def isHawaiian(w):
-    pattern = r'^([hklmnpw][aeiou]+)+([\'][aeiou])?$' # mehrere Vokale hintereinander erlaubt! T_T
+    # mehrere Vokale hintereinander erlaubt! Pattern GENAU anschauen T_T
+    pattern = r'^([aeiou]*[hklmnpw][aeiou]+)*(\'[aeiou]*[hklmnpw]*[aeiou])*$'
     match = re.match(pattern, w, re.IGNORECASE)
-    result = bool(match)        
-    print(f'Uebergeben: {w:25s} Match: {match.group() if match else "":25s} Result: {result}')
+    result = bool(match)
+    # print(f'Uebergeben: {w:25s} Match: {match.group() if match else "":25s} Result: {result}')        
     return result
 
-if __name__=="__main__":
-    isHawaiian("Hawai'i")                   #wahr
-    isHawaiian("Wahine")                    #wahr
-    isHawaiian("wikiwiki")                  #wahr    
-    isHawaiian("Humuhumunukunukuapua'a")    #wahr
+def extractHi(iterable):
+    for sentence in iterable:
+        words = re.findall(r'[\w\']+', sentence, re.IGNORECASE)  # Extrahiere Wörter ohne Satzzeichen
+        for word in words:
+            if isHawaiian(word):
+                yield word
 
-    isHawaiian("Qui'juno")      #falsch
-    isHawaiian("Alaaf")         #falsch
-    isHawaiian("lekker")        #falsch
-    isHawaiian("Pu'kuuli")      #falsch
-    isHawaiian("ohamak'uluu")   #falsch
+if __name__=="__main__":
+    isHawaiian("Aloha")
+    isHawaiian("Hawai'i")
+    isHawaiian("Wahine")
+    isHawaiian("wikiwiki")
+    isHawaiian("Humuhumunukunukuapua'a")
+
+    isHawaiian("Qui'juno")
+    isHawaiian("Alaaf")
+    isHawaiian("lekker")
+    isHawaiian("Pu'kuuli")
+    isHawaiian("ohamak'uluu")
+    it = extractHi(["Eine Wahine sagt", "Maika'i no au!"])
+    for i in it: 
+        print(i)
+
+    it = extractHi(("Hau'oli", "la hanau!", "Happy", "Birthday!"))
+    for i in it:
+        print(i)
